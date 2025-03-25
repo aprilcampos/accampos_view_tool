1ea20dac1c2930ac6588826c43b0395703a11b18
April Camille Campos <aprilcamille.campos@gmail.com>
2025-03-25 14:47:15 +0800

    ViewTool initial commit

---
+++ lib/accampos_view_tool.rb
@@ -0,0 +1,7 @@
+# frozen_string_literal: true
+
+require_relative "accampos_view_tool/version"
+require_relative "accampos_view_tool/renderer"
+
+module AccamposViewTool
+end
---
+++ lib/accampos_view_tool/version.rb
@@ -0,0 +1,5 @@
+# frozen_string_literal: true
+
+module AccamposViewTool
+  VERSION = "0.1.0"
+end
